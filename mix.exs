3fcb09113d58fe793b13175bc2805453d7a10df8
Aleksandr Sysoev <sanchous.ok@gmail.com>
2024-08-21 20:11:13 +0700

    Try to downgrade minimum Elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RedPool.MixProject do
     [
       app: :red_pool,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
